8920835c68ddaf7f2b4a3bad245c66a9e08bb12e
Hans-Kristian Koren <hanse@koren.im>
2019-04-25 10:42:20 +0200

    Update dotfiles

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # my dotfiles
 
-```bash
-git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-source "$HOME/.homesick/repos/homeshick/homeshick.sh"
-homeshick clone hanse/dotfiles
-homeshick link dotfiles
-```
+Run `setup_machine.sh` on a new machine.
--- setup_machine.sh
+++ setup_machine.sh
@@ -6,11 +6,18 @@ source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone hanse/dotfiles
 homeshick link dotfiles
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 export OS=`uname -s`
 
 if [ "$OS" = "Darwin" ]
 then
   defaults write com.apple.finder QLEnableTextSelection -bool true && killall Finder
+  defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+  
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew bundle
 fi
+
+curl https://raw.githubusercontent.com/Schniz/fnm/master/.ci/install.sh | bash
